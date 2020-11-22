6cd41746973d89dd0ccf7af6212df32975b02a07
Nonthicha2545 <nunt.nonthicha@gmail.com>
2020-11-22 21:33:34 +0700

    Add project files.

---
+++ Hw28/Source.cpp
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+struct patz
+{
+	int i = 0;
+}ez;
+
+void covertBin(int dec)
+{
+	static int numBin[128];
+	while (dec != 0)
+	{
+		numBin[ez.i] = dec % 2;
+		dec /= 2;
+		ez.i++;
+	}
+	for (ez.i; ez.i >= 0; ez.i--)
+	{
+		printf("%d", numBin[ez.i]);
+	}
+}
+
+int main()
+{
+	int numDec;
+	printf("Enter positive integer : ");
+	scanf("%d", &numDec);
+	if (numDec > 0 || numDec != NULL)
+	{
+		covertBin(numDec);
+	}
+
+	return 0;
+}
